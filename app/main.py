7ca405f557152691a49b46abc521605aabce5b8a
CmilAmaya <mamayaro@unal.edu.co>
2025-09-28 11:47:56 -0500

    bugfix: changes in dockerfile

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth_routes
+import os
 
 app = FastAPI(title="Users authentication service")
 app.include_router(auth_routes.router, prefix="/users_authentication_path", tags=["users_authentication"])
 
 app.add_middleware(
-	CORSMiddleware,
-	allow_origins=["*"],
-	allow_credentials=True,
-	allow_methods=["*"],
-	allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 if __name__ == "__main__":
-	import uvicorn
-	uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))  
+    uvicorn.run(app, host="0.0.0.0", port=port)
